6605927f138c91dc12406d388e08cfc722d0eea9
Mariusz Sowa <mariuszsowa11@gmail.com>
2017-11-28 19:12:08 +0100

    docker

--- src/main/kotlin/com/jalgoarena/security/config/JwtSettings.kt
+++ src/main/kotlin/com/jalgoarena/security/config/JwtSettings.kt
@@ -2,8 +2,10 @@ package com.jalgoarena.security.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
+@EnableSwagger2
 @ConfigurationProperties(prefix = "jalgoarena.security.jwt")
 open class JwtSettings {
     open var tokenExpirationTime: Int? = null
